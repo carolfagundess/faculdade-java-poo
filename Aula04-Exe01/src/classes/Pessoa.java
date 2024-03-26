2b9f989b50a0d74d16ced0799c2deced5e7a0d2a
Carol Fagundes <carolinarftk@gmail.com>
2024-03-25 22:22:52 -0300

    salvando antes de por try catch no codigo

---
+++ Aula04-Exe01/src/classes/Pessoa.java
@@ -0,0 +1,67 @@
+package classes;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Carolina Fagundes
+ */
+public class Pessoa {
+
+    private String nome;
+    private String email;
+    private String telefone;
+    private LocalDate dataNascimento;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome) {
+        this.nome = nome;
+    }
+
+    public Pessoa(String nome, String email, String telefone, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", dataNascimento=" + dataNascimento + '}';
+    }
+
+}
